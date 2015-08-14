8dde5cb9f0f7133bb35090d3bcc8a7bd0b960307
Dan <dcandy287@gmail.com>
2015-08-14 19:16:10 +1000

    change indentation

--- push_thermal.py
+++ push_thermal.py
@@ -15,13 +15,13 @@ from firefly import authentication
 
 #define a capture method/fucntion for the lepton module
 def capture(flip_v = False, device = "/dev/spidev0.1"):
-  with Lepton(device) as l:
-    a,_ = l.capture()
-  if flip_v:
-    cv2.flip(a,0,a)
-  cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX)
-  np.right_shift(a, 8, a)
-  return np.uint8(a)
+    with Lepton(device) as l:
+        a,_ = l.capture()
+        if flip_v:
+            cv2.flip(a,0,a)
+        cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX)
+        np.right_shift(a, 8, a)
+        return np.uint8(a)
   
   
 
@@ -55,15 +55,15 @@ frames_sent = 0
 
 while True:
 
-	thermal_image = capture(flip_v = options.flip_v, device = options.device)
-	
-	ret, thermal_frame = cv2.imencode('.jpg', thermal_image)
-	
-	thermal_bytes = thermal_frame.tobytes()
-	
-	frame_len = len(thermal_bytes)
+    thermal_image = capture(flip_v = options.flip_v, device = options.device)
+    
+    ret, thermal_frame = cv2.imencode('.jpg', thermal_image)
+    
+    thermal_bytes = thermal_frame.tobytes()
+    
+    frame_len = len(thermal_bytes)
 
-	if frame_len > MAX_PACKET_SIZE:
+    if frame_len > MAX_PACKET_SIZE:
         # need to split the frame into fragments
         num_fragments = int(math.ceil(1.0*frame_len/MAX_PACKET_SIZE))
         print "Splitting frame into %d fragments" % num_fragments
